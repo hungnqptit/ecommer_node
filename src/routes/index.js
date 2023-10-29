9d030a0ff6f498f6dc7b2c9465075670c316831d
Ngo Quang Hung <hungnqptit@gmail.com>
2023-10-29 10:13:38 +0700

    Section 6: Api Key

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 "use strict";
 
 const express = require("express");
+const { apiKey, permission } = require("../auth/checkAuth");
 
 const router = express.Router();
 
+router.use(apiKey);
+router.use(permission("0000"));
 router.use("/v1/api", require("./access"));
 // router.get("/", (req, res) => {
 //   return res.status(200).json({
